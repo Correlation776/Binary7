0cc3a74e5c0c0109549ab924d7811c62b3fec215
Correlation776 <thgrtdstrr@gmail.com>
2023-11-20 16:04:53 +0300

    yeah

--- Binary7Tests/UnitTest1.cs
+++ Binary7Tests/UnitTest1.cs
@@ -5,15 +5,21 @@ namespace Binary7Tests;
 public class Tests
 {
     [Test]
-    public void BasicTest()
+    public void BasicTest1()
     {
         Assert.That(Program.BinaryGenerator(10, 2), Is.EqualTo(new List<int> {3, 5, 6, 9}));
     }
     
     [Test]
+    public void BasicTest2()
+    {
+        Assert.That(Program.BinaryGenerator(3600, 11), Is.EqualTo(new List<int> {2047, 3071, 3583}));
+    }
+    
+    [Test]
     public void BigNumbersTest()
     {
-        Assert.That(Program.BinaryGenerator(100000, 16), Is.EqualTo(new List<int> {65535, 98303}));
+        Assert.That(Program.BinaryGenerator(1000000000, 29), Is.EqualTo(new List<int> {536870911, 805306367, 939524095}));
     }
     
     [Test]
